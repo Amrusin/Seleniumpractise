2d8a560d9743284d060ccb261a2c7c8b9851628a
Amrutha <sindhu10.ammu@gmail.com>
2024-04-20 13:41:49 +0530

    third

--- src/test/java/JenkinsPractiseTest.java
+++ src/test/java/JenkinsPractiseTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -6,6 +8,13 @@ public class JenkinsPractiseTest {
 	@Test
 	public void Test1()
 	{
+		WebDriver driver= null;
+		String browser=System.getProperty("browser");
+		if(browser.equals("chrome"))
+		{
+			driver=new ChromeDriver();
+			
+		}
 		Reporter.log("********Login**********", true);
 		Reporter.log("********Logout**********", true);
 		//hi
