526cb74a1eaf33163a0993c733092e0d7d2a4875
Amrutha <sindhu10.ammu@gmail.com>
2024-04-20 12:58:24 +0530

    First change

---
+++ src/test/java/JenkinsPractiseTest.java
@@ -0,0 +1,11 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsPractiseTest {
+
+	@Test
+	public void Test1()
+	{
+		Reporter.log("********Login**********", true);
+	}
+}
